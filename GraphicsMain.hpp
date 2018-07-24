06ae6a0d115e695becdbf6e01386cc6f36dd8d16
Joseph L. Ngoma <30995482+Kid-Seven-7@users.noreply.github.com>
2018-07-24 09:11:15 +0200

    Update GraphicsMain.hpp

--- GraphicsMain.hpp
+++ GraphicsMain.hpp
@@ -16,12 +16,15 @@
 #include <SFML/Window.hpp>
 #include <iostream>
 
+template <typename T>
 class GraphicsMain
 {
     public:
-        virtual int         getWidth() const = 0;
-        virtual int         getHeight() const = 0;
-        virtual std::string getName() const = 0;
-        virtual void        createSfmlWindow() = 0;
+        virtual int         getWidth() const = 0; //not needed
+        virtual int         getHeight() const = 0; //not needed
+        virtual std::string getName() const = 0; //not needed
+        virtual void        createWindow() = 0;
+        virtual void        destroyWindow(T window) = 0;
+        virtual void        updateWindow(T window) = 0;
 };
 #endif
