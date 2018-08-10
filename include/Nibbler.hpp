df548928419fa4f614afa1ee1ba7adbaf5581875
Joseph L. Ngoma <testing.jo.wtc@gmail.com>
2018-08-10 13:46:54 +0200

    Removed needless methods and members

--- include/Nibbler.hpp
+++ include/Nibbler.hpp
@@ -14,37 +14,31 @@ struct Part {
 	int x, y;
 	int nextX, nextY;
 	int prevX, prevY;
-	bool isHead;
-	bool isTail;
-	bool canMove;
 };
 
 class Snake_class {
 	private:
-		int length, direction;
+		int direction;
 		std::vector<Part> Snake;
-		bool isAlive;
 	public:
-		Snake_class();
 		Snake_class(int width, int height);
 		~Snake_class();
-
-		int 		getLength();
-		int 		getX(int index);
-		int 		getY(int index);
-		int 		getNextX(int index);
-		int 		getNextY(int index);
-		bool 		getType(int index);
-		void 		setDirection(int dirValue);
-		int 		getDirection();
-		void 		moveSnake();
-		void 		addPart();
-		bool 		collision();
+		//ints
+		int getDirection();
+		int getX(int index);
+		int getY(int index);
+		int getNextX(int index);
+		int getNextY(int index);
+		//voids
+		void addPart();
+		void moveSnake();
+		void setDirection(int dirValue);
+		void setVector(std::vector<Part> &Snake);
+		void generateFood(int &width, int &height, int &food_x, int &food_y, std::vector<Part> &Snake);
+		//bools
+		bool collision();
+		//misc
 		std::vector<Part> getVector();
-		void 		setVector(std::vector<Part> &Snake);
-		void 		reset();
-		void        generateFood(int width, int height, int & food_x, int & food_y, std::vector<Part> &Snake);
-        // void        generateObj(int width, int height, int & obj_x, int & obj_y, std::vector<Part> &Snake);
 };
 
 extern	"C"
